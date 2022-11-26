a18c27176bd3fcb339579482e8ca0ede62fed783
Gregory <elvismabisi@gmail.com>
2022-11-26 20:06:27 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+// 
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
+console.log(multiply);
+
+//the math..ceil rounds off the random number to the highest
+let random = Math.ceil(Math.random());
+
+//24 % 5 return 4.8 math.round() rounds it to 5 math.trunc()truncates it to
+const num3 = 24;
+const num4 = 5;
+let mod = Math.trunc(num3 % num4);
+console.log(mod);
+
+let max = Math.max(11, 12, 20)
